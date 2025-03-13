4a6f141086f446c845a82407f67251ca3bd0a45e
AVINASH <144657036+avinashkurhe@users.noreply.github.com>
2025-03-14 00:20:58 +0530

    Update Navbar.js

--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -3,14 +3,39 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className='container nav_bar'>
-      <div className='left nav_item'>Portfolio</div>
-      <div className='right nav_bar'>
-        <Link className='nav_item' to='/'>Home</Link>
-        <Link className='nav_item' to='/education'>Education</Link>
-        <Link className='nav_item' to='/skills'>Skills</Link> {/* corrected path */}
-        <Link className='nav_item' to='/projects'>Projects</Link> {/* corrected path */}
-        <Link className='nav_item' to='/contact'>Contact</Link>
+    <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="navbar-brand text-light"><b>ASK</b></div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav ms-auto">
+            <Link className="nav-item nav-link text-light" to="/">
+              Home
+            </Link>
+            <Link className="nav-item nav-link text-light" to="/education">
+              Education
+            </Link>
+            <Link className="nav-item nav-link text-light" to="/skills">
+              Skills
+            </Link>
+            <Link className="nav-item nav-link text-light" to="/projects">
+              Projects
+            </Link>
+            <Link className="nav-item nav-link text-light" to="/contact">
+              Contact
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
