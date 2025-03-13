9d458248248be5a432af2646fa45e635b143e2c7
AVINASH <144657036+avinashkurhe@users.noreply.github.com>
2025-03-14 00:05:15 +0530

    Update Education.js

--- src/componants/Education.js
+++ src/componants/Education.js
@@ -3,30 +3,31 @@ import education from './data/education.json';
 
 function Education() {
   return (
-    <React.Fragment>
-      <div className='container ex' id='education'>
-        <h1>EDUCATION</h1>
-        {
-          education.map((data) => (
-            <div key={data.id} className='ex-item text-center my-5'>
-              <div className='left'>
-                <img src={`/assets/${data.imageSrc}`} alt='img'></img>
-              </div>
-              <div className='right'>
-                <h2>{data.role}</h2>
-                <h4>
-                  <span style={{color: "yellowgreen"}}>{data.startDate}{"  "}{data.endDate}</span>
-                  {" "}
-                  <span style={{color: "yellow"}}>{data.location}</span>
-                </h4>
-                <h5 style={{color: "yellow"}}>{data.college[0]}</h5>
-                <h5 style={{color: "yellow"}}>{data.college[1]}</h5>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-    </React.Fragment>
+    <section className='container education-section mt-4' id='education'>
+      <h1 className='text-center text-warning mb-4'>EDUCATION</h1>
+      
+      {education.map((data) => (
+        <div key={data.id} className='ex-item d-flex flex-wrap align-items-center my-4'>
+          
+          {/* Left Section - Image */}
+          <div className='left'>
+            <img src={`/assets/${data.imageSrc}`} alt='Education' className='edu-img' />
+          </div>
+
+          {/* Right Section - Details */}
+          <div className='right text-center text-md-start'>
+            <h2 className='text-primary fw-bold'>{data.role}</h2>
+            <h4>
+              <span className='text-success'>{data.startDate} - {data.endDate}</span> |
+              <span className='text-warning ms-2'>{data.location}</span>
+            </h4>
+            <h5 className='text-warning'>{data.college[0]}</h5>
+            <h5 className='text-warning'>{data.college[1]}</h5>
+          </div>
+
+        </div>
+      ))}
+    </section>
   );
 }
 
