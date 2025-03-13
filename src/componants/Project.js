045b404565b6a63ffd3b45b7f89a1f00d132bb47
AVINASH <144657036+avinashkurhe@users.noreply.github.com>
2025-03-14 00:21:54 +0530

    Update Project.js

--- src/componants/Project.js
+++ src/componants/Project.js
@@ -1,45 +1,32 @@
-import React from 'react'
-import project from './data/projects.json'
+import React from 'react';
+import project from './data/projects.json';
 
 function Project() {
   return (
-    <>
-      <div classNameName='container projects my-3' id='project'>
-        <h1>PROJECTS</h1>
-        <div className="row d-flex justify-content-center align-content-center">
-          {project.map((data) => (
-            <>
-              <div key={data.id} className="my-4 col-sm-6 col-md-4 col-lg-3 mx-4">
+    <section className='container projects my-5' id='project'>
+      <h1 className='text-center text-warning mb-4'>PROJECTS</h1>
 
-                <div className="card bg-dark text-light" style={{
-                  width: '18rem', border: '1px solid yellow',boxShadow:'5px 5px 10px 10px rgba(101,175,10,0.5)'
-                }}>
-                  <div className='img d-flex justify-content-center align-content-center p-3'>
-                    <img src={data.imageSrc} className="card-img-top" alt="..."
-                      style={{
-                        width: '250px',
-                        height: '200px',
-                        border: '2px solid yellow',
-                        borderRadius: '10px'
-                      }} />
-
-                  </div>
-                  <div className="card-body text-center">
-                    <h5 className="card-title">{data.title}</h5>
-                    <p className="card-text">{data.description}</p>
-                    <a href={data.demo} className="btn btn-primary mx-3">Demo</a>
-                    <a href={data.source} className="btn btn-warning">Code</a>
-
-                  </div>
+      <div className="row justify-content-center">
+        {project.map((data) => (
+          <div key={data.id} className="col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div className="project-card">
+              <div className="project-img">
+                <img src={data.imageSrc} alt={data.title} />
+              </div>
+              <div className="project-content">
+                <h5>{data.title}</h5>
+                <p>{data.description}</p>
+                <div className="project-buttons">
+                  <a href={data.demo} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Demo</a>
+                  <a href={data.source} className="btn btn-warning" target="_blank" rel="noopener noreferrer">Code</a>
                 </div>
               </div>
-            </>
-          ))}
-        </div>
+            </div>
+          </div>
+        ))}
       </div>
-
-    </>
-  )
+    </section>
+  );
 }
 
-export default Project
+export default Project;
